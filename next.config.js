152b4a647ddbc1ee1459818e30bcc42c68ac3bf4
Nick Cox <nc@jacobsenforcement.com>
2023-04-16 15:16:49 +0100

    init commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+// next.config.js
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        pathname: '/u9ozd7i029ja/**',
+      },
+    ],
+  },
+};
---
+++ pages/posts/[slug].js
@@ -0,0 +1,67 @@
+import { client } from '../../lib/contentful';
+import Image from 'next/image';
+
+export async function getStaticPaths() {
+  const res = await client.getEntries({ content_type: 'portfolioPosts' });
+
+  const paths = res.items.map((post) => ({
+    params: { slug: post.fields.slug },
+  }));
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  const res = await client.getEntries({
+    content_type: 'portfolioPosts',
+    'fields.slug': params.slug,
+  });
+
+  const post = res.items[0];
+
+  return {
+    props: {
+      title: post.fields.title,
+      description: post.fields.description,
+      disciplines: post.fields.disciplines,
+      weblink: post.fields.weblink,
+      content: post.fields.content,
+      thumbnailUrl: `https:${post.fields.thumbnail.fields.file.url}`,
+      thumbnailAlt: post.fields.thumbnail.fields.title,
+      thumbnailWidth: post.fields.thumbnail.fields.file.details.image.width,
+      thumbnailHeight: post.fields.thumbnail.fields.file.details.image.height,
+    },
+  };
+}
+
+export default function PostPage({
+  title,
+  description,
+  disciplines,
+  weblink,
+  content,
+  thumbnailUrl,
+  thumbnailAlt,
+  thumbnailWidth,
+  thumbnailHeight,
+}) {
+  return (
+    <>
+      <div>
+        <h1>{title}</h1>
+        <Image
+          src={thumbnailUrl}
+          alt={thumbnailAlt}
+          width={thumbnailWidth}
+          height={thumbnailHeight}
+        />
+        <p>{description}</p>
+        <p>Disciplines: {disciplines.join(', ')}</p>
+        <p>
+          Web Link: <a href={weblink}>{weblink}</a>
+        </p>
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
+    </>
+  );
+}
