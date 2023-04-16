2b2ac6ff5e56804b0b661c655e53dd382360a167
Nick Cox <nc@jacobsenforcement.com>
2023-04-16 16:25:35 +0100

    fix

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -8,7 +8,7 @@ export async function getStaticPaths() {
     params: { slug: post.fields.slug },
   }));
 
-  return { paths, fallback: false };
+  return { paths, fallback: true };
 }
 
 export async function getStaticProps({ params }) {
