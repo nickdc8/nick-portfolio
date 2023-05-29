c6942af1fe87d714ee4eb7f1c3d3f45e7771a00e
Nick Cox <nc@jacobsenforcement.com>
2023-05-29 13:37:06 +0100

    font added

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,17 @@
 import Nav from './Nav';
 import Footer from './Footer';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
 
 export default function RootLayout({ children }) {
   return (
-    <div className='flex flex-col min-h-screen max-w-screen-xl mx-auto px-4'>
+    <div
+      className={`${inter.variable} font-sans flex flex-col min-h-screen max-w-screen-xl mx-auto px-4`}
+    >
       <Nav />
       <main className='flex-1'>{children}</main>
       <Footer />
