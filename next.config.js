87b9945d0508cce4ef6885357968fe36e896d569
Nick Cox <nickdc8@gmail.com>
2023-08-18 13:25:02 +0100

    npm updatE

--- next.config.js
+++ next.config.js
@@ -7,6 +7,19 @@ module.exports = nextConfig;
 
 // next.config.js
 module.exports = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    let modularizeImports = null;
+    config.module.rules.some((rule) =>
+      rule.oneOf?.some((oneOf) => {
+        modularizeImports = oneOf?.use?.options?.nextConfig?.modularizeImports;
+        return modularizeImports;
+      })
+    );
+    if (modularizeImports?.['@headlessui/react'])
+      delete modularizeImports['@headlessui/react'];
+    return config;
+  },
   images: {
     remotePatterns: [
       {
