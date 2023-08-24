83709bdf550270db96a7d1515b3f09e716763f16
Nick Cox <nickdc8@gmail.com>
2023-08-24 22:10:14 +0100

    npm update

---
+++ components/Placeholder.jsx
@@ -0,0 +1,7 @@
+const PlaceholderSkeleton = () => {
+  return (
+    <div className='animate-pulse bg-gray-300 dark:bg-gray-700 h-40 w-full rounded-md' />
+  );
+};
+
+export default PlaceholderSkeleton;
