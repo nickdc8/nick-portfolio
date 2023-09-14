c3927f2996e7f5dbb24b0de6b2fb9e7717c4ec58
Nick Cox <nickdc8@gmail.com>
2023-09-14 18:27:35 +0100

    accessibility fix

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer className='mt-20 mb-10'>
       <div className='max-w-7xl flex justify-between items-center space-x-5 mx-auto mt-10 py-4'>
-        <p className=' dark:text-slate-500 text-slate-500 text-sm'>
+        <p className=' dark:text-slate-400 text-slate-500 text-sm'>
           &copy; {new Date().getFullYear()} All Rights Reserved.
         </p>
         <ThemeSwitch />
