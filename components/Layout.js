9998855f3d764be58ec362cdc7142fca7b16f7c4
Nick Cox <nickdc8@gmail.com>
2023-09-18 08:41:54 +0100

    analytics added

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import Nav from './Nav';
 import Footer from './Footer';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -13,7 +14,10 @@ export default function RootLayout({ children }) {
       className={`${inter.variable} font-sans flex flex-col min-h-screen max-w-screen-xl mx-auto px-4`}
     >
       <Nav />
-      <main className='flex-1'>{children}</main>
+      <main className='flex-1'>
+        {children}
+        <Analytics />
+      </main>
       <Footer />
     </div>
   );
